623b70287f94144716362a7ae44650afc1167bfb
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-11 08:54:30 +0200

    initializing Css file of the Navbar giving the initial properties of the UI

---
+++ src/components/SettingsNavbar/SettingsNavbar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import styles from "./SettingsNavbar.module.css"
+/**
+ * Component to render the navbar of the settings page 
+ * @author Abdalla Mahmoud
+ * 
+ * @component 
+ */
+export default class SettingsNavbar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/pages/Settings/Settings.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+/**
+ * Component to render the settings page with its different routes
+ * @author Abdalla Mahmoud
+ *
+ * @component
+ *
+ */
+export default class Settings extends Component {
+  render() {
+    return <div></div>;
+  }
+}
