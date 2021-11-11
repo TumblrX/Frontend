173cca43f14b0f126ee3b6e8f12258030e404e21
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-11 09:19:53 +0200

    creating nav-bar slots adding css properites of them ,it will be updated again

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import SettingsNavbar from './components/SettingsNavbar/SettingsNavbar';
 
-import {
-  NotFound,
-  Dashboard,
-  Explore,
-  Inbox,
-  New,
-  LoginPage,
-} from './pages/pages';
-import { NavBar, ExploreLayout } from './components/Layouts/Layouts';
 const App = () => {
   return (
-    <Fragment>
-      <NavBar />
-      <Switch>
-        <Route exact path='/'>
-          <Redirect to='/dashboard' />
-        </Route>
-        <Route exact path='/explore'>
-          <ExploreLayout>
-            <Explore />
-          </ExploreLayout>
-        </Route>
-        <Route exact path='/dashboard'>
-          <Dashboard />
-        </Route>
-        <Route exact path='/inbox'>
-          <Inbox />
-        </Route>
-        <Route exact path='/new'>
-          <New />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Fragment>
+    <div>
+      <SettingsNavbar/>
+    </div>
   );
 };
 
--- src/components/SettingsNavbar/SettingsNavbar.jsx
+++ src/components/SettingsNavbar/SettingsNavbar.jsx
@@ -10,7 +10,22 @@ export default class SettingsNavbar extends Component {
     render() {
         return (
             <div>
-                
+                <div className={styles["nav-bar-slot"]}>
+                    <div >Account</div>
+                    <div>The essentials</div>
+                </div>
+                <div className={styles["nav-bar-slot"]}>
+                    <div>Dashboard</div>
+                    <div>Appearance options,text editor</div>
+                </div>
+                <div className={styles["nav-bar-slot"]}>
+                    <div>Notifications</div>
+                    <div>Via email &amp; mobile </div>
+                </div>
+                <div className={styles["nav-bar-slot"]}>
+                    <div>Privacy</div>
+                    <div>Personalization and data management</div>
+                </div>
             </div>
         )
     }
