308d9c12d75f230b003bfe24fad8fdfa0c9a9f80
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2021-11-11 03:41:46 +0200

    Updating the new post page

--- src/pages/New/New.js
+++ src/pages/New/New.js
@@ -1,11 +1,64 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import classes from './New.module.scss';
+import { newPostIcons } from '../../components/UI/Icons';
+const {
+  IoText,
+  AiFillCamera,
+  ImQuotesLeft,
+  FaLink,
+  BsFillChatDotsFill,
+  ImHeadphones,
+  TiVideo,
+} = newPostIcons;
 const New = () => {
   return (
-    <Fragment>
+    <div className={classes.new}>
       <div className={classes.overlay}></div>
-      <div className={classes.content}>Content</div>
-    </Fragment>
+      <div className={classes.content}>
+        <div className={classes.text}>
+          <div>
+            <IoText />
+          </div>
+          <span>Text</span>
+        </div>
+        <div className={classes.photo}>
+          <div>
+            <AiFillCamera />
+          </div>
+          <span>Photo</span>
+        </div>
+        <div className={classes.quote}>
+          <div>
+            <ImQuotesLeft />
+          </div>
+          <span>Quote</span>
+        </div>
+        <div className={classes.link}>
+          <div>
+            <FaLink />
+          </div>
+          <span>Link</span>
+        </div>
+        <div className={classes.chat}>
+          <div>
+            <BsFillChatDotsFill />
+          </div>
+          <span>Chat</span>
+        </div>
+        <div className={classes.audio}>
+          <div>
+            <ImHeadphones />
+          </div>
+          <span>Audio</span>
+        </div>
+        <div className={classes.video}>
+          <div>
+            <TiVideo />
+          </div>
+          <span>Video</span>
+        </div>
+      </div>
+    </div>
   );
 };
 
