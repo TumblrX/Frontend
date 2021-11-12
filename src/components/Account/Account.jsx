3d1a3601c774759aba51a3c856eba989f77c5889
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-12 21:21:38 +0200

    👌Done
    -  add functional documentation for the email section
    
    ✔️To do
    
    - Add Password section as a separated component

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -4,6 +4,7 @@ import pen from "../../assets/Images/pencil-64x64.png";
 import axios from "axios";
 import EmailSection from "./EmailSection/EmailSection";
 import PasswordSection from "./PasswordSection/PasswordSection";
+import SecuritySection from "./SecuritySection/SecuritySection";
 /**
  * Component to render the Account settings in the Settings page
  * @author Abdalla Mahmoud
@@ -315,47 +316,9 @@ export class Account extends Component {
           <PasswordSection/>
 
           {/*  */}
-          <div className={styles["security-box"]}>
-            <div className={styles["title"]}>Security</div>
-            <div className={styles["security-section"]}>
-              <div style={{ display: "flex", marginBottom: "30px" }}>
-                <input
-                  type="checkbox"
-                  style={{ marginTop: "6px", marginRight: "6px" }}
-                />
-                <div className={styles["security-description"]}>
-                  <div className={styles["section-message"]}>
-                    Email me about account activity
-                  </div>
-                  <div className={styles["user-message"]}>
-                    You will receive an email when someone logs into your
-                    account or a new app is authorized
-                  </div>
-                </div>
-              </div>
-              <div style={{ display: "flex", marginBottom: "30px" }}>
-                <input
-                  type="checkbox"
-                  style={{ marginTop: "6px", marginRight: "6px" }}
-                />
-                <div>
-                  <div className={styles["section-message"]}>
-                    Two-factor authentication
-                  </div>
-                  <div className={styles["user-message"]}>
-                    Enabling two factor authentication makes it extra difficult
-                    for anyone other than you to access your account.
-                    <a href="/learnmore" className={styles["anchor"]}>
-                      {" "}
-                      Learn more.
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <hr />
-
+          
+          <SecuritySection/>
+          
           <div className={styles["filtering-section"]}>
             <div className={styles["title"]}>Filtering</div>
             <div style={{ width: "80%" }}>
---
+++ src/components/Account/SecuritySection/SecuritySection.jsx
@@ -0,0 +1,52 @@
+import styles from "../Account.module.css";
+
+import React from "react";
+
+function SecuritySection() {
+  return (
+    <>
+      <div className={styles["security-box"]}>
+        <div className={styles["title"]}>Security</div>
+        <div className={styles["security-section"]}>
+          <div style={{ display: "flex", marginBottom: "30px" }}>
+            <input
+              type="checkbox"
+              style={{ marginTop: "6px", marginRight: "6px" }}
+            />
+            <div className={styles["security-description"]}>
+              <div className={styles["section-message"]}>
+                Email me about account activity
+              </div>
+              <div className={styles["user-message"]}>
+                You will receive an email when someone logs into your account or
+                a new app is authorized
+              </div>
+            </div>
+          </div>
+          <div style={{ display: "flex", marginBottom: "30px" }}>
+            <input
+              type="checkbox"
+              style={{ marginTop: "6px", marginRight: "6px" }}
+            />
+            <div>
+              <div className={styles["section-message"]}>
+                Two-factor authentication
+              </div>
+              <div className={styles["user-message"]}>
+                Enabling two factor authentication makes it extra difficult for
+                anyone other than you to access your account.
+                <a href="/learnmore" className={styles["anchor"]}>
+                  {" "}
+                  Learn more.
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </>
+  );
+}
+
+export default SecuritySection;
