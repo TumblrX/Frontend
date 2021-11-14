f499436830566dc5b675f69bbffb65e47dde4b08
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-14 04:06:04 +0200

    👌Done
    -  customize the notification section, give the sections their width height etc .
    
    ✔️To-do
    
    1. give the elements their font-size colors and so on

--- src/components/Notifications/Notifictions.jsx
+++ src/components/Notifications/Notifictions.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import styles from "./Notifications.module.css";
 import pen from "../../assets/Images/pencil-64x64.png";
+import userPhoto from "../../assets/Images/myphoto.jpg"
 export class Notifictions extends Component {
   render() {
     return (
@@ -8,11 +9,11 @@ export class Notifictions extends Component {
         <h1 className={styles["header"]}>Account</h1>
         <hr />
         <div className={styles["notification-section"]}>
-          <div style={{ display: "flex" }}>
-            <img src="" alt="" />
+          <div className={styles["user-info"]} style={{ display: "flex" }}>
+            <img src={userPhoto} alt="" className={styles["user-img"]} />
             <div>Thebrownboy</div>
           </div>
-          <div>
+          <div className={styles["edit-section"]}>
             Some notification and some emails
             <img className={styles["icon-photo"]} src={pen} alt="" />
           </div>
