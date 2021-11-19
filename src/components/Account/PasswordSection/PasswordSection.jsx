d409a32e3f7975c2139f54073ed0c4b478fb8b5c
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-20 00:09:23 +0200

    👌Done
    - converting PasswordSection.jsx  to functional component  there are some important notes to take into consideration ⚠️
      1. Hooks have been used instead of States in in this functional component
      2. All the data that is fetched from the Server is set in the useEffect Hook that have no parameters in its dependencies list and the axios call is made inside it and the State of the hook is set inside this method .. as it is know that the setState  functions is an asynchronous so the changes on it does not reflect on the same time , it will reflect in the next time the component is rendered by react but  the changes can be seen by using useEffect method , As in class component when the setState methods called a call back function is used to do any code after changing the state , here the useEffect method is used instead , but what if you fetch data from a server the state should not be put in the dependencies list , to see the changes, the value of the state should be given to the input field which is done here so no errors has been found  as the error that happen in the Interface Section file which will be uploaded later .. the problem will be discussed in details in the commit that will  convert the InterfaceSection.jsx to functional  component
      3. ✔️ converting these components has helped in some performance issues that have been faced in the past
    
    ✔️To-do
    
    1. adding validations on the User inputs
    2. adding unit testing  for the rest components
    3. adding more tests on password and email components

--- src/components/Account/PasswordSection/PasswordSection.jsx
+++ src/components/Account/PasswordSection/PasswordSection.jsx
@@ -1,49 +1,24 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
 import styles from "../Account.module.css";
 import pen from "../../../assets/Images/pencil-64x64.png";
 import axios from "axios";
-
-/**
- * Component to render the Password section on the Account section
- *
- * @author Abdalla Mahmoud
- * @Component
- */
-export class PasswordSection extends Component {
-  /**
-   * @constructor
-   *
-   * @public
-   *
-   * @param {object} props any props sent from parent component
-   */
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      /**
-       * string for previous password
-       * @type {string}
-       */
-      password: "",
-      /**
-       * string for password that the user will enter to confirm changing email
-       * @type {string}
-       */
-      confirmedPassword: "",
-      /**
-       * string for the new password
-       * @type {string}
-       */
-      newPassword: "",
-      /**
-       * confirmed new password
-       * @type {string}
-       */
-      newConfirmedPassword: "",
-    };
-  }
+function PasswordSection(props) {
+  const [passwordInfo, updateInfo] = useState({
+    password: "",
+    confirmedPassword: "",
+    newPassword: "",
+    newConfirmedPassword: "",
+  });
+  useEffect(() =>{
+    axios
+      .get("http://localhost:3000/users/1")
+      .then((response) => {
+        updateInfo({ ...passwordInfo, password: response.data.password });
+        console.log(passwordInfo);
+      })
+      .catch();
+  }, []);
   /**
    * this function handle the click on the save button in the email section
    * @function
@@ -51,7 +26,7 @@ export class PasswordSection extends Component {
    * @param {event} event
    * @returns {void} return nothing , it just a click event handler
    */
-  formAction = (event) => {
+  let formAction = (event) => {
     /**
      * @type {Array<Element>}
      * get save buttons
@@ -61,16 +36,16 @@ export class PasswordSection extends Component {
     );
 
     if (event.target === saveButtons[1]) {
-      if (this.state.confirmedPassword !== this.state.password) {
+      if (passwordInfo.confirmedPassword !== passwordInfo.password) {
         document.getElementsByClassName(
           `${styles["error-current-password"]}`
         )[0].style.visibility = "unset";
       } else if (
-        this.state.newPassword.length < 10 ||
-        this.state.password === this.state.newPassword
+        passwordInfo.newPassword.length < 10 ||
+        passwordInfo.password === passwordInfo.newPassword
       ) {
         // one condition for test
-        if (this.state.newPassword.length < 10)
+        if (passwordInfo.newPassword.length < 10)
           document.getElementsByClassName(
             `${styles["error-new-password"]}`
           )[0].style.visibility = "unset";
@@ -78,13 +53,15 @@ export class PasswordSection extends Component {
           document.getElementsByClassName(
             `${styles["error-new-password"]}`
           )[1].style.visibility = "unset";
-      } else if (this.state.newPassword !== this.state.newConfirmedPassword) {
+      } else if (
+        passwordInfo.newPassword !== passwordInfo.newConfirmedPassword
+      ) {
         document.getElementsByClassName(
           `${styles["error-confirm-password"]}`
         )[0].style.visibility = "unset";
       } else {
-        let sentData = { password: this.state.newPassword };
-        this.props.sendData(sentData);
+        let sentData = { password: passwordInfo.newPassword };
+        props.sendData(sentData);
       }
     }
   };
@@ -96,7 +73,7 @@ export class PasswordSection extends Component {
    * @return {void} returns nothing it just an event handler
    */
 
-  cancelButtonClick = (event) => {
+  let cancelButtonClick = (event) => {
     document.querySelectorAll(".error-message").forEach((element) => {
       element.style.visibility = "hidden";
       element.style.transition = "none";
@@ -135,34 +112,13 @@ export class PasswordSection extends Component {
 
     document.querySelectorAll(`form`)[0].style.pointerEvents = "all";
   };
-  /**
-   * @function
-   * @param {void }
-   * @returns {void}
-   * retreive the data from the backend when the component mounted
-   */
-  componentDidMount() {
-    axios
-      .get("http://localhost:3000/users/1")
-      .then((response) => {
-        this.data = response.data;
-        this.previousData = response.data;
-        this.setState(() => {
-          return {
-            email: response.data.email,
-            password: response.data.password,
-          };
-        });
-      })
-      .catch();
-  }
 
   /**
    * this function handle the event handler on edit button icon
    * @param {event} event
    * @return {void} return nothing it just an event handler
    */
-  iconClick = (event) => {
+  let iconClick = (event) => {
     document.querySelectorAll(".error-message").forEach((element) => {
       element.style.transition = "0.5s .1s linear";
     });
@@ -215,7 +171,7 @@ export class PasswordSection extends Component {
    * @param {event} event
    * @returns {void}
    */
-  changeInput = (event) => {
+  let changeInput = (event) => {
     document.querySelectorAll(".error-message").forEach((element) => {
       // if the user enter invalid input then try to enter new values
       element.style.visibility = "hidden";
@@ -225,118 +181,100 @@ export class PasswordSection extends Component {
       element.style.visibility = "hidden";
     });
     if (event.target.id === "currentpassword") {
-      this.setState(() => {
-        return { confirmedPassword: event.target.value };
-      });
+      updateInfo({ ...passwordInfo, confirmedPassword: event.target.value });
     } else if (event.target.id === "newpassword") {
-      this.setState(() => {
-        return { newPassword: event.target.value };
-      });
+      updateInfo({ ...passwordInfo, newPassword: event.target.value });
     } else if (event.target.id === "confirmpassword") {
-      this.setState(() => {
-        return { newConfirmedPassword: event.target.value };
-      });
+      updateInfo({ ...passwordInfo, newConfirmedPassword: event.target.value });
     } else {
-      this.setState(() => {
-        return { confirmedPassword: event.target.value };
-      });
+      updateInfo({ ...passwordInfo, confirmedPassword: event.target.value });
     }
+    console.log(passwordInfo);
   };
 
-  /**
-   * this function is responsible render the Email section
-   * @returns {jsx} return jsx to be renderd
-   */
-  render() {
-    return (
-      <>
-        <div  data-testid="password-section"className={styles["password-box"]}>
-          <div className={styles["title"]}>Password</div>
-          <div
-            className={styles["dots"]}
-            onClick={this.iconClick}
-            data-testid="dots"
-          >
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-          <div className={`${styles["input-fields"]} ${styles.hidden}`} data-testid="password-section-input-field">
-            <input
-              id="currentpassword"
-              type="password"
-              name=""
-              placeholder="Current Password"
-              value={this.state.confirmedPassword}
-              onChange={this.changeInput}
-              data-testid="currentpassword-box"
-            />
-            <div
-              className={`${styles["error-current-password"]} error-message `}
-            >
-              Please Enter your password Correctly
-            </div>
-            <input
-              id="newpassword"
-              type="password"
-              placeholder="New Password"
-              value={this.state.newPassword}
-              onChange={this.changeInput}
-              data-testid="newpassword-box"
-            />
-            <div className={`${styles["error-new-password"]} error-message`}>
-              Please Enter Strong Password
-            </div>
-            <div className={`${styles["error-new-password"]} error-message`}>
-              Please Don't enter you previous Password
-            </div>
-            <input
-              id="confirmpassword"
-              type="password"
-              placeholder="Confirm Password"
-              value={this.state.newConfirmedPassword}
-              onChange={this.changeInput}
-              data-testid="confirmpassword-box"
-            />
-            <div
-              className={`${styles["error-confirm-password"]} error-message`}
-            >
-              Please Enter Identical Passwords
-            </div>
-
-            <div id="password-section-buttons" >
-              <button
-                onClick={this.cancelButtonClick}
-                className={styles["cancel-button"]}
-                type="button"
-              >
-                cancel
-              </button>
-              <button
-                type="button"
-                onClick={this.formAction}
-                className={styles["save-button"]}
-              >
-                save
-              </button>
-            </div>
-          </div>
-
-          <img
-            src={pen}
-            onClick={this.iconClick}
-            className={styles["icon-photo"]}
-            alt=" can't load "
-            data-testid="password-edit-button"
-          />
+  return (
+    <>
+      <div data-testid="password-section" className={styles["password-box"]}>
+        <div className={styles["title"]}>Password</div>
+        <div className={styles["dots"]} onClick={iconClick} data-testid="dots">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-        <hr />
-      </>
-    );
-  }
+        <div
+          className={`${styles["input-fields"]} ${styles.hidden}`}
+          data-testid="password-section-input-field"
+        >
+          <input
+            id="currentpassword"
+            type="password"
+            name=""
+            placeholder="Current Password"
+            value={passwordInfo.confirmedPassword}
+            onChange={changeInput}
+            data-testid="currentpassword-box"
+          />
+          <div className={`${styles["error-current-password"]} error-message `}>
+            Please Enter your password Correctly
+          </div>
+          <input
+            id="newpassword"
+            type="password"
+            placeholder="New Password"
+            value={passwordInfo.newPassword}
+            onChange={changeInput}
+            data-testid="newpassword-box"
+          />
+          <div className={`${styles["error-new-password"]} error-message`}>
+            Please Enter Strong Password
+          </div>
+          <div className={`${styles["error-new-password"]} error-message`}>
+            Please Don't enter you previous Password
+          </div>
+          <input
+            id="confirmpassword"
+            type="password"
+            placeholder="Confirm Password"
+            value={passwordInfo.newConfirmedPassword}
+            onChange={changeInput}
+            data-testid="confirmpassword-box"
+          />
+          <div className={`${styles["error-confirm-password"]} error-message`}>
+            Please Enter Identical Passwords
+          </div>
+
+          <div id="password-section-buttons">
+            <button
+              onClick={cancelButtonClick}
+              className={styles["cancel-button"]}
+              type="button"
+            >
+              cancel
+            </button>
+            <button
+              type="button"
+              onClick={formAction}
+              className={styles["save-button"]}
+            >
+              save
+            </button>
+          </div>
+        </div>
+
+        <img
+          src={pen}
+          onClick={iconClick}
+          className={styles["icon-photo"]}
+          alt=" can't load "
+          data-testid="password-edit-button"
+        />
+      </div>
+      <hr />
+    </>
+  );
 }
 
 export default PasswordSection;
