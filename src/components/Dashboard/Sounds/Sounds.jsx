42f0b1c44d9e7324be03f47dd11f14fec0ec2131
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-20 01:43:49 +0200

    converting Sounds.jsx into functional component

--- src/components/Dashboard/Sounds/Sounds.jsx
+++ src/components/Dashboard/Sounds/Sounds.jsx
@@ -1,32 +1,34 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../Dashboard.module.css";
 import axios from "axios";
-/**
- * @type {boolean}
- * this var will have the current state of the checkbox
- */
-let checkboxState;
 
 /**
- * component to render the interface on the sound section
- * @author Abdalla Mahmoud
- * @component
+ * this function is responsible render the sounds  section
+ * @function
+ * @returns {jsx} return jsx to be renderd
  */
-export class Sounds extends Component {
+function Sounds(props) {
+  const [checkBoxState, setState] = useState({ state: null });
   /**
    * @function
    * @param {void }
    * @returns {void }
    * check if the check button has been clicked and send its value to the server
    */
-  checkBoxClick = () => {
-    checkboxState = !checkboxState;
-
-    let sentData = {
-      messagingSounds: checkboxState,
-    };
-    this.props.sendData(sentData);
+  const checkBoxClick = () => {
+    // on click set the state with the inversion of the previous state
+    setState((prevState) => {
+      return { state: !prevState.state };
+    });
   };
+
+  useEffect(() => {
+    if (checkBoxState.state === null) return;
+    let sentData = {
+      messagingSounds: checkBoxState.state,
+    };
+    props.sendData(sentData);
+  }, [checkBoxState, props]);
 
   /**
    * @function
@@ -34,48 +36,43 @@ export class Sounds extends Component {
    * @returns {void}
    * retreive the data from the backend when the component mounted
    */
-  componentDidMount = () => {
+  const componentDidMount = () => {
     axios
       .get("http://localhost:3000/users/1")
       .then((response) => {
         document.querySelectorAll(`input[type="checkbox"]`)[1].checked =
           response.data.messagingSounds;
-        checkboxState = response.data.messagingSounds;
+
+        setState({ state: response.data.messagingSounds });
       })
       .catch((err) => {});
   };
-  /**
-   * this function is responsible render the sounds  section
-   * @function
-   * @returns {jsx} return jsx to be renderd
-   */
-  render() {
-    return (
-      <>
-        <div className={styles["section"]}>
-          <div className={styles["title"]}>Sounds</div>
-          <div className={styles["section-form"]}>
-            <div style={{ display: "flex", marginBottom: "30px" }}>
-              <input
-                type="checkbox"
-                style={{ marginTop: "6px", marginRight: "6px" }}
-                onClick={this.checkBoxClick}
-                data-testid="messageing-sounds-checkbox"
-              />
-              <div className={styles["description"]}>
-                <div className={styles["section-message"]}>
-                  Messaging sounds
-                </div>
-                <div className={styles["user-message"]}>
-                  Assorted 'shling!' and 'fwip!' noises in messaging.
-                </div>
+
+  useEffect(componentDidMount, []);
+  return (
+    <>
+      <div className={styles["section"]}>
+        <div className={styles["title"]}>Sounds</div>
+        <div className={styles["section-form"]}>
+          <div style={{ display: "flex", marginBottom: "30px" }}>
+            <input
+              type="checkbox"
+              style={{ marginTop: "6px", marginRight: "6px" }}
+              onClick={checkBoxClick}
+              data-testid="messageing-sounds-checkbox"
+              value={checkBoxState.state}
+            />
+            <div className={styles["description"]}>
+              <div className={styles["section-message"]}>Messaging sounds</div>
+              <div className={styles["user-message"]}>
+                Assorted 'shling!' and 'fwip!' noises in messaging.
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default Sounds;
