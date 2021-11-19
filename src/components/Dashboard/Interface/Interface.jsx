430e00d265307dac0e0f6c73a24261d6698e1184
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-11-20 01:32:37 +0200

    👌Done
    - ### Very Important Commit ⚠️⚠️⚠️⚠️
    
      1. ​	This commit is the most important commit in this branch implementing The interface Section in the Dashboard Section in the settings
      2. what makes this commit very important is the functionality and the concept that is used to make this section work as it is expected
      3. implementing this section by using hooks add some difficulties  in the concept not in the code
      4. sending data to the server is responsive  immediately with the user action on the check box, the data will be send every time the user click on the check box so how it have been handled
      5. first of all the nature of the useState function is asynchronous  , that means that the state does not changed immediately the function is called, all the code under it will be executed with previous data no the up-to-date one so sending data directly  after changing it will not cause error but will add wrong data to the server
      6. solving this problem is simple in code but a little bit hard in concept
      7. this is the steps that is followed to solve this issue
         1. firstly initialize the value of the state with null , but it will make react asserting an warning saying that props or state has been assigned a null value , to over come this problem is so simple by adding the null value as a value of a key in the state `const [checkBoxState, setState] = useState({ state: null });`
         2. secondly you want to fetch the data from the server whenever the component is rendered so you will use `useEffect` hook with empty dependencies list  to mimic the `componentDidMount() `function, update the checkcheck box checked property with the value coming from the server then update the state with the value coming from the server .. here is the KEY of the problem
         3. knowing that the setState function is asynchronous the value will not updated immediately so another `useEffect` hook should be used to sense the changes on the state of the box ..
         4. the other `useEffect` will check if the state is null "the first time after mounting " it will do nothing , and if not it will send the state to the server
      8. This is the follow of code (the component is mounted )=> (the data is fetched from the server )=>(the state will be updated with the value coming from the server )=>(the `useEffect` hook that senses the changes of the state will be fired )=>(if the data is null return )=> (the first `useEffect` hook will not called again )=>(the second one will be called when any change happens )
    
    ✔️To-do
    
    1. adding validations on the User inputs
    2. adding unit testing  for the rest components
    3. adding more tests on password and email components

--- src/components/Dashboard/Interface/Interface.jsx
+++ src/components/Dashboard/Interface/Interface.jsx
@@ -1,90 +1,88 @@
-import axios from "axios";
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../Dashboard.module.css";
-/**
- * this variable works as a state , there is no mean to put state here in this
- * component , for performance concerns
- * @type {boolean}
- */
-let checkboxState;
+import axios from "axios";
+function Interface(props) {
+  // the state is supposed to be null not false and not true
+  // why , but react assert a waring that warns from using the state  value with null
+  // so the state will be object with a key equal to null
+  const [checkBoxState, setState] = useState({ state: null });
 
-/**
- * component to render the interface on the Dashboard settings
- * @author Abdalla Mahmoud
- * @component
- */
-
-class Interface extends Component {
   /**
    * @function
    * @param {void }
    * @returns {void }
    * check if the check button has been clicked and send its value to the server
    */
-  checkBoxClick = () => {
-    checkboxState = !checkboxState;
-    /**
-     * @type{object } sentData
-     * object for the data that will be sent to the server
-     */
-
-    let sentData = {
-      enableEndLessScrolling: checkboxState,
-    };
-    this.props.sendData(sentData);
+  const checkBoxClick = () => {
+    // on click set the state with the inversion of the previous state
+    setState((prevState) => {
+      return { state: !prevState.state };
+    });
   };
 
+  useEffect(() => {
+    // value will be null in the first call the the other useEffect state
+    // TAKE CARE  the setState function is an async function so we use useEffect method
+    // to see any change happens to the state
+    if (checkBoxState.state === null) return;
+    let sentData = {
+      enableEndLessScrolling: checkBoxState.state,
+    };
+    // I will send the data when the checkbox is clicked
+    // so every update in the state should be sent to the server
+    // so I will listen here for any change done in the checkboxstate and send it to the server .
+    // it is different from the EmailSection.jsx and Password because these sections have submit 
+    // button 
+    props.sendData(sentData);
+  }, [checkBoxState, props]);
   /**
    * @function
    * @param {void}
    * @returns {void}
    * retreive the data from the backend when the component mounted
    */
-  componentDidMount = () => {
+  const componentDidMount = () => {
     axios
       .get("http://localhost:3000/users/1")
       .then((response) => {
-        console.log(response.data.enableEndLessScrolling);
         document.querySelectorAll(`input[type="checkbox"]`)[0].checked =
           response.data.enableEndLessScrolling;
-        checkboxState = response.data.enableEndLessScrolling;
+
+        setState({ state: response.data.enableEndLessScrolling });
       })
       .catch((err) => {});
   };
-  /**
-   * this function is responsible render the interface section
-   * @function
-   * @returns {jsx} return jsx to be renderd
-   */
-  render() {
-    return (
-      <>
-        <div className={styles["section"]}>
-          <div className={styles["title"]}>Interface</div>
-          <div className={styles["section-form"]}>
-            <div style={{ display: "flex", marginBottom: "30px" }}>
-              <input
-                type="checkbox"
-                style={{ marginTop: "6px", marginRight: "6px" }}
-                onClick={this.checkBoxClick}
-                data-testid="enable-endless-scrolling-checkbox"
-              />
-              <div className={styles["description"]}>
-                <div className={styles["section-message"]}>
-                  Eanble endless scrolling
-                </div>
-                <div className={styles["user-message"]}>
-                  Surf your dashboard page-by-page instead of as an
-                  infinitely-scrolling feed. To update the URL for each page in
-                  your browser, you'll also need to disable Best Stuff First.
-                </div>
+
+  useEffect(componentDidMount, []);
+
+  return (
+    <>
+      <div className={styles["section"]}>
+        <div className={styles["title"]}>Interface</div>
+        <div className={styles["section-form"]}>
+          <div style={{ display: "flex", marginBottom: "30px" }}>
+            <input
+              type="checkbox"
+              style={{ marginTop: "6px", marginRight: "6px" }}
+              onClick={checkBoxClick}
+              data-testid="enable-endless-scrolling-checkbox"
+              value={checkBoxState}
+            />
+            <div className={styles["description"]}>
+              <div className={styles["section-message"]}>
+                Eanble endless scrolling
+              </div>
+              <div className={styles["user-message"]}>
+                Surf your dashboard page-by-page instead of as an
+                infinitely-scrolling feed. To update the URL for each page in
+                your browser, you'll also need to disable Best Stuff First.
               </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
 }
 
 export default Interface;
