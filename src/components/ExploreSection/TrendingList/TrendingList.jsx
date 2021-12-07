eb1544fd3e28ebbb5a5c6eb105ec8c3205c4f865
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-07 19:19:49 +0200

    ### done
    - customizing the trending card
    
    ### to-do
    
    - adding Posts and Customize the Trending section

--- src/components/ExploreSection/TrendingList/TrendingList.jsx
+++ src/components/ExploreSection/TrendingList/TrendingList.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./scss/TrendingList.module.scss";
+import TrendCard from "./TrendCard";
 function TrendingList() {
   return (
     <div className={styles["trending-list"]}>
       <ul className={styles["odd-items"]}>
+          <TrendCard/>
       </ul>
       <ul className={styles["even-items"]}>
       </ul>
