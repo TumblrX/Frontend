c76188c836d320a628b0ec35251b961082b1a1ce
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-07 13:40:33 +0200

    ### done
    - Adding Following list in the Aside bar
    
    ### to-do
    
    - adding the checkout list

--- src/components/ExploreAside/ExploreAside.jsx
+++ src/components/ExploreAside/ExploreAside.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
-import styles from "./scss/ExploreAside.module.scss"
+import React from "react";
+import styles from "./scss/ExploreAside.module.scss";
+import img from "../../assets/Images/Project_128-09.jpg";
 function ExploreAside() {
-    return (
-        <div className={styles["explore-aside"]}>
-            Explore Aside 
+  return (
+    <div className={styles["explore-aside"]}>
+      <div className={styles["following-list"]}>
+        <div className={styles["header"]}>
+          <div>Following</div>
+          <div className={styles["edit-button"]}>Edit</div>
         </div>
-    )
+        <ul className={styles["following-items"]}>
+          <li>
+            <div>
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <div>#health &amp; fitneess</div>
+              <div>
+                <span style={{ fontWeight: "bold" }}>90</span> recent posts{" "}
+              </div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <div>#health &amp; fitneess</div>
+              <div>
+                <span style={{ fontWeight: "bold" }}>90</span> recent posts{" "}
+              </div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <div>#health &amp; fitneess</div>
+              <div>
+                <span style={{ fontWeight: "bold" }}>90</span> recent posts{" "}
+              </div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src={img} alt="" />
+            </div>
+            <div>
+              <div>#health &amp; fitneess</div>
+              <div>
+                <span style={{ fontWeight: "bold" }}>90</span> recent posts{" "}
+              </div>
+            </div>
+          </li>
+        </ul>
+        <div className={styles["show-more-tags"]}>
+            Show More Tags 
+        </div>
+      </div>
+      <div>Checkout list </div>
+    </div>
+  );
 }
 
-export default ExploreAside
+export default ExploreAside;
