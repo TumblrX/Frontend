15abd7d50d040c973907c12e8493d86ac3fc1bde
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-07 10:42:02 +0200

    ### done
    - Adding suggestion list of the explore recommended for you section and customizing the suggestion card
    
    ### to-do
    
    - Adding the main section of explore and the suggestion posts .

--- src/components/ExploreSection/ExploreSection.jsx
+++ src/components/ExploreSection/ExploreSection.jsx
@@ -1,44 +1,55 @@
 import React from "react";
 import styles from "./scss/ExploreSection.module.scss";
+import img1 from "../../assets/Images/Project_128-09.jpg"
 function ExploreSection() {
   return (
     <div className={styles["explore-section"]}>
-      
-        <div className={styles["explore-navbar"]}>
-          <div className={styles["explore-selection"]}>
-            <span>For You 💖</span>
-            <span>Trending 🚀</span>
-            <span>Staff Picks 🌟</span>
-            <span>More </span>
-          </div>
-          <div>
-            <span className={styles["grid-icons"]} tabindex="-1">
-              <svg
-                viewBox="0 0 24 24"
-                width="24"
-                height="24"
-                fill="RGB(255,255,255,.65)"
-              >
-                <rect width="8" height="10" x="3" y="3" rx="1"></rect>
-                <rect width="8" height="6" x="13" y="3" rx="1"></rect>
-                <rect width="8" height="10" x="13" y="11" rx="1"></rect>
-                <rect width="8" height="6" x="3" y="15" rx="1"></rect>
-              </svg>
-            </span>
-            <span className={styles["grid-icons"]} tabindex="-1">
-              <svg
-                viewBox="0 0 24 24"
-                width="23"
-                height="23"
-                fill="RGB(255,255,255,.65)"
-              >
-                <rect width="16" height="4" x="4" y="2" rx="1"></rect>
-                <rect width="16" height="4" x="4" y="18" rx="1"></rect>
-                <rect width="16" height="8" x="4" y="8" rx="1"></rect>
-              </svg>
-            </span>
-          </div>
+      <div className={styles["explore-navbar"]}>
+        <div className={styles["explore-selection"]}>
+          <span>For You 💖</span>
+          <span>Trending 🚀</span>
+          <span>Staff Picks 🌟</span>
+          <span>More </span>
         </div>
+        <div>
+          <span className={styles["grid-icons"]} tabindex="-1">
+            <svg
+              viewBox="0 0 24 24"
+              width="24"
+              height="24"
+              fill="RGB(255,255,255,.65)"
+            >
+              <rect width="8" height="10" x="3" y="3" rx="1"></rect>
+              <rect width="8" height="6" x="13" y="3" rx="1"></rect>
+              <rect width="8" height="10" x="13" y="11" rx="1"></rect>
+              <rect width="8" height="6" x="3" y="15" rx="1"></rect>
+            </svg>
+          </span>
+          <span className={styles["grid-icons"]} tabindex="-1">
+            <svg
+              viewBox="0 0 24 24"
+              width="23"
+              height="23"
+              fill="RGB(255,255,255,.65)"
+            >
+              <rect width="16" height="4" x="4" y="2" rx="1"></rect>
+              <rect width="16" height="4" x="4" y="18" rx="1"></rect>
+              <rect width="16" height="8" x="4" y="8" rx="1"></rect>
+            </svg>
+          </span>
+        </div>
+      </div>
+
+      <div className={styles["suggestion-list"]}>
+        <div className={styles["suggestion-card"]}>
+          <div>#Photography</div>
+          <div className={styles["card-imgs-container"]}>
+            <img src={img1} alt="" />
+            <img src={img1} alt="" />
+          </div>
+          <button>Follow</button>
+        </div>
+      </div>
     </div>
   );
 }
