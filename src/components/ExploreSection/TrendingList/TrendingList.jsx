6a40a20db63c8ebb2d453375e79d5e8d77a94490
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-07 20:00:08 +0200

    ### done
    - finalizing the trending section
    
    ### to-do
    
    - Suggestion list arrow must be sticky instead of fixed

--- src/components/ExploreSection/TrendingList/TrendingList.jsx
+++ src/components/ExploreSection/TrendingList/TrendingList.jsx
@@ -4,11 +4,28 @@ import TrendCard from "./TrendCard";
 function TrendingList() {
   return (
     <div className={styles["trending-list"]}>
-      <ul className={styles["odd-items"]}>
-          <TrendCard/>
-      </ul>
-      <ul className={styles["even-items"]}>
-      </ul>
+      <div>
+        <ul className={styles["odd-items"]}>
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+        </ul>
+        <ul className={styles["even-items"]}>
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+          <TrendCard />
+        </ul>
+      </div>
     </div>
   );
 }
