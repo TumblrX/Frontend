03811a9b4ff177cf0ec5e938624f008fbaa468d9
YousefElshabrawy <yousefatefelshabrawy@gmail.com>
2021-12-07 18:18:16 +0200

    Linting the Post.js

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,4 +1,10 @@
-/* eslint-disable */
+/* eslint-disable consistent-return */
+/* eslint-disable array-callback-return */
+/* eslint-disable prefer-const */
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable no-empty */
+/* eslint-disable max-len */
 import React from 'react';
 import classes from './Post.module.scss';
 import { Link } from 'react-router-dom';
@@ -7,11 +13,12 @@ import { FaRegComment, FaRegHeart } from 'react-icons/fa';
 import { IoGitCompareSharp } from 'react-icons/io5';
 import { RiDeleteBinLine, RiShareForwardLine, RiEdit2Line } from 'react-icons/ri';
 import getYoutubeVideoId from '../../helpers/getYoutubeVideoId';
-const Post = function ({data}) {
-  const { id, blog, content } = data;
+
+const Post = function ({ data }) {
+  const { content } = data;
   const postContent = content.map((contentBlock) => {
-    if(contentBlock.type === 'text'){
-      let {subtype, text, formatting} =  contentBlock;
+    if (contentBlock.type === 'text') {
+      let { subtype, text, formatting } = contentBlock;
       // if(formatting){
       //   // If the formatting array is not empty, we need to format the text first
       //   let boldCount=0;
@@ -33,99 +40,101 @@ const Post = function ({data}) {
       //   })
       //   console.log(text);
       // }
-      if(!subtype){
+      if (!subtype) {
         // return <p dangerouslySetInnerHTML={{__html: text}}></p>;
         return <p className={classes.text}>{text}</p>;
-      }else{
-        if(subtype==='heading1'){
-          return <h1>{text}</h1>
-        }
-        else if(subtype==='heading2'){
-          return <h2>{text}</h2>
-        }
-        else if(subtype==='quirky'){
-
-        }
-        else if(subtype==='quote'){
-          return  (
-            <q cite="">{text}</q>
-          )
-        }
-        else if(subtype==='indented'){
-          return  (
-            <figure>
-              <blockquote cite="">
-                  <p>Words can be like X-rays, if you use them properly—they’ll go through anything. You read and you’re pierced.</p>
-              </blockquote>
-              <figcaption>—Aldous Huxley, <cite>Brave New World</cite></figcaption>
-            </figure>
-          )
-        }
-        else if(subtype==='chat'){
-          text=text.replace(text.slice(formatting[0].start,formatting[0].end), `<b>${text.slice(formatting[0].start,formatting[0].end)}</b>`);
-          return(
-            <p className={classes.chat} dangerouslySetInnerHTML={{__html: text}}></p>
-          )
-        }
-        else if(subtype==='ordered-list-item'){
-          const items=['item1','itemq2','item3']; 
-          return (
-            <ol>
-              {items.map(item=><li>{item}</li>)}
-            </ol>
-          ) 
-        }
-        else if(subtype==='unordered-list-item'){
-          return (
-            <ul>
-              <li></li>
-            </ul>
-          )
-        }
       }
-    }else if(contentBlock.type==='image'){
-      return(
+      if (subtype === 'heading1') {
+        return <h1>{text}</h1>;
+      }
+      if (subtype === 'heading2') {
+        return <h2>{text}</h2>;
+      }
+      if (subtype === 'quirky') {
+
+      } else if (subtype === 'quote') {
+        return (
+          <q cite="">{text}</q>
+        );
+      } else if (subtype === 'indented') {
+        return (
+          <figure>
+            <blockquote cite="">
+              <p>roperly—they’ll go through anything. You read and you’re pierced.</p>
+            </blockquote>
+            <figcaption>
+              —Aldous Huxley,
+              <cite>Brave New World</cite>
+            </figcaption>
+          </figure>
+        );
+      } else if (subtype === 'chat') {
+        text = text.replace(text.slice(formatting[0].start, formatting[0].end), `<b>${text.slice(formatting[0].start, formatting[0].end)}</b>`);
+        return (
+          <p className={classes.chat} dangerouslySetInnerHTML={{ __html: text }} />
+        );
+      } else if (subtype === 'ordered-list-item') {
+        const items = ['item1', 'itemq2', 'item3'];
+        return (
+          <ol>
+            {items.map((item) => <li>{item}</li>)}
+          </ol>
+        );
+      } else if (subtype === 'unordered-list-item') {
+        return (
+          <ul>
+            <li />
+          </ul>
+        );
+      }
+    } else if (contentBlock.type === 'image') {
+      return (
         <figure className={classes.image}>
-            <img src={contentBlock.url} alt={contentBlock.altText} width={contentBlock.width} height={contentBlock.height}/>
+          <img
+            src={contentBlock.url}
+            alt={contentBlock.altText}
+            width={contentBlock.width}
+            height={contentBlock.height}
+          />
           <figcaption>{contentBlock.caption}</figcaption>
         </figure>
-      )
-    }else if(contentBlock.type==='link'){
+      );
+    } else if (contentBlock.type === 'link') {
       return (
-       <div className={classes.link}>
+        <div className={classes.link}>
           <a href={contentBlock.url}>
             <h3>{contentBlock.title}</h3>
             <p>{contentBlock.author}</p>
           </a>
           <p>{contentBlock.description}</p>
-          { contentBlock.poster && <img src={contentBlock.poster.url} width={contentBlock.poster.width} height={contentBlock.poster.height}/>}
+          { contentBlock.poster && <img src={contentBlock.poster.url} width={contentBlock.poster.width} height={contentBlock.poster.height} />}
         </div>
-      )
-    }else if(contentBlock.type==='audio'){
+      );
+    } else if (contentBlock.type === 'audio') {
       let audio;
-      if(contentBlock.provider==='soundcloud'){
-        audio=<iframe width="100%" height="166" src={contentBlock.url} frameborder="no"></iframe>
-      }else {
-        //Here provider is tumblrx
+      if (contentBlock.provider === 'soundcloud') {
+        audio = <iframe width="100%" height="166" src={contentBlock.url} frameBorder="no" />;
+      } else {
+        // Here provider is tumblrx
       }
-      return(
+      return (
         <div className={classes.audio}>
           {audio}
         </div>
-       
-      ) 
-    }else if(contentBlock.type==='video'){
+
+      );
+    } else if (contentBlock.type === 'video') {
       let video;
-      if(contentBlock.provider==='youtube'){
-        video=<iframe width="100%" height="315" src={`//www.youtube.com/embed/${getYoutubeVideoId( contentBlock.url)}`} frameborder="0" allowfullscreen></iframe>;
-      }else {
-        //Here provider is tumblrx
+      if (contentBlock.provider === 'youtube') {
+        video = <iframe width="100%" height="315" src={`//www.youtube.com/embed/${getYoutubeVideoId(contentBlock.url)}`} frameBorder="0" allowFullScreen />;
+      } else {
+        // Here provider is tumblrx
       }
-      return(
+      return (
         <div className={classes.video}>
           {video}
         </div>
-      ) 
+      );
     }
   });
   return (
