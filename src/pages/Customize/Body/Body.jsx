00cb09ccc974bb67f36cdaa61b77b81da14d345e
Taher Mohamed Ahmed Saad <15126@stemegypt.edu.eg>
2021-12-18 04:54:25 +0200

    uploading photos in customize page

--- src/pages/Customize/Body/Body.jsx
+++ src/pages/Customize/Body/Body.jsx
@@ -5,25 +5,46 @@ import React, { useState } from 'react';
 import style from './Body.module.scss';
 
 const Body = function () {
-  const [avatar1, setAvatar1] = useState('https://assets.tumblr.com/images/default_avatar/sphere_open_128.png');
-  const [imageData, sstImageData] = useState('');
-  const changeAvatar1 = (e) => {
+  const [avatar, setAvatar] = useState('https://assets.tumblr.com/images/default_avatar/sphere_open_128.png');
+  const [headerImage, setHeaderImage] = useState('https://assets.tumblr.com/images/default_header/optica_pattern_10_focused_v3.png?_v=eafbfb1726b334d86841955ae7b9221c');
+  const [avatarData, sestavatarData] = useState('');
+  const [headerImageData, setHeaderImageData] = useState('');
+  const changeAvatar = (e) => {
     const reader = new FileReader();
     reader.onload = () => {
       if (reader.readyState === 2) {
-        setAvatar1(reader.result);
+        setAvatar(reader.result);
       }
     };
     reader.readAsDataURL(e.target.files[0]);
-    sstImageData(e.target.files[0]);
-
-    console.log(e.target.files[0]);
-    console.log(imageData);
+    sestavatarData(e.target.files[0]);
+  };
+  const changeHeaderImage = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setHeaderImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+    setHeaderImageData(e.target.files[0]);
   };
   return (
     <div>
-      <input type="file" onChange={changeAvatar1} />
-      <img src={avatar1} alt="" accept="image/*" />
+      <input type="file" onChange={changeAvatar} />
+      <input type="file" onChange={changeHeaderImage} />
+      <div style={{
+        background: `url(${headerImage})`, backgroundSize: 'cover', height: '300px', width: '100vw',
+      }}
+      />
+      <img
+        src={avatar}
+        alt=""
+        accept="image/*"
+        style={{
+          height: '100px', width: '100px', borderRadius: '100%', transform: 'translate(450px, -70px)',
+        }}
+      />
     </div>
   );
 };
