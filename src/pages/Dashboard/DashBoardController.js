ce927644a242ab516f449944b8374f6b2980f637
omar214 <omarmohamed2322000@gmail.com>
2021-12-21 01:55:17 +0200

    dashboard integration done

--- src/pages/Dashboard/DashBoardController.js
+++ src/pages/Dashboard/DashBoardController.js
@@ -1,82 +1,59 @@
-/* eslint-disable array-callback-return */
-/* eslint-disable no-lone-blocks */
-/* eslint-disable no-console */
-/* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
-/* eslint-disable no-else-return */
 /* eslint-disable react/no-array-index-key */
 import React from 'react';
 import styles from './Dashboard.module.scss';
 import Post from '../../components/Post/Post';
+import logo from '../../assets/Images/avatar.png';
 
-const showPosts = (posts, pageNum, isInfinte) => {
-// console.log('show posts is called --> ', posts);
-// console.log('show posts is called --> ', posts.length);
-// let firstPost;
-// let lastPost;
-// if (isInfinte) {
-//   firstPost = 0;
-//   lastPost = posts.length;
-// } else {
-//   lastPost = pageNum * 10;
-//   firstPost = lastPost - 10;
-// }
-// eslint-disable-next-line block-scoped-var
-// posts.slice(0, 2).map((post, index) => (
-// if (typeof posts !== 'undefined') {
-  console.log('show posts is called');
-  posts.map((post, index) => {
-    // console.log('index ', index);
-    // console.log('post ', post);
-    // console.log('index -->', index);
-    <div className={`${styles.post} ${styles.row}`} key={index} data-testid={`testPost${index}`}>
+const showPosts = (posts, pageNum, isInfinte, pageNumPosts) => {
+  let firstPost;
+  let lastPost;
+  if (isInfinte) {
+    firstPost = 0;
+    lastPost = posts.length;
+  } else {
+    lastPost = pageNum * pageNumPosts;
+    firstPost = lastPost - pageNumPosts;
+  }
+  return posts.slice(firstPost, lastPost).map((post, index) => (
+    <div className={`${styles.post} ${styles.row}`} key={index}>
+      <div className={styles.logo}>
+        <img src={logo} alt="avatar" className={styles.avatar} />
+      </div>
+      <div className={styles.postDatailes}>
+        <Post data={post} />
+      </div>
+    </div>
+  ));
+};
+
+const getOnePost = (posts) => {
+  const i = Math.floor(Math.random() * (posts.length - 1));
+  const post = posts[i];
+  return (
+    <div className={`${styles.post} `}>
       <div className={styles.logo} />
       <div className={styles.postDatailes}>
         <Post data={post} />
       </div>
-    </div>;
-  });
+    </div>
+  );
 };
-
-// } else {
-//   return (
-//     <div />
-//   );
-// }
-
-const getOnePost = (posts) => {
-  // const i = Math.floor(Math.random() * (posts.length - 1));
-  const i = 0;
-  const post = posts[i];
-  // if (typeof post !== 'undefined') {
-  //   console.log('one post', post);
-  //   return (
-  //     <div className={`${styles.post}`}>
-  //       <div className={styles.logo} />
-  //       <div className={styles.postDatailes}>
-  //         <Post data={post} />
-  //       </div>
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <div />
-  //   );
-  // }
-
-  // if (post) {
-  //   return (
-  //     <div className={`${styles.post}`}>
-  //       <div className={styles.logo} />
-  //       <div className={styles.postDatailes}>
-  //         <Post data={post} />
-  //       </div>
-  //     </div>
-  //   );
-  // } else {
-  //   return (
-  //     <div />
-  //   );
-  // }
-};
-export { showPosts, getOnePost };
+const showBlogsForYou = (exploreBlogs) => exploreBlogs.map((blog, index) => (
+  <div className={`${styles.exploreBlog} ${styles.row}`} key={index}>
+    <div className={styles.blogAvatar}>
+      avatar
+    </div>
+    <div className={styles.blogTitle}>
+      <h4> Blog Title </h4>
+      <p className={styles.p}> Fav1 Fav2 Fav3 </p>
+    </div>
+    <div className={styles.blogFollow}>
+      <button className={styles.follow}> Follow </button>
+    </div>
+    <div className={styles.blogExit}>
+      <button className={styles.exit}> &times; </button>
+    </div>
+  </div>
+));
+export { showPosts, getOnePost, showBlogsForYou };
