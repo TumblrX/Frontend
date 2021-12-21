fbbe7526d7af33c83930ee27ea2558277a255dc1
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-21 03:07:03 +0200

    ## Done ✔️
    - Separating the `iconClick`function from the Component file to the controller file .
    
    ## To-do 📝
    
    - Separate all the controller functions from the component file .

--- src/components/Account/PasswordSection/PasswordSection.jsx
+++ src/components/Account/PasswordSection/PasswordSection.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable max-len */
 /* eslint-disable react/destructuring-assignment */
 import React, { useEffect } from "react";
-import { changeInput } from "./PasswordSectionController";
+import { changeInput, iconClick } from "./PasswordSectionController";
 import styles from "../Account.module.css";
 import pen from "../../../assets/Images/pencil-64x64.png";
 import api from "../../../api/api";
@@ -24,7 +24,6 @@ import { sendPasswordData } from "./PasswordSectionServices";
 const PasswordSection = function (props) {
   const { password, confirmedPassword, newPassword, newConfirmedPassword } =
     useSelector((state) => state.passwordInfo);
-  const dispatch = useDispatch();
   /**
    * this function handle the click on the save button in the email section
    * @type {function}
@@ -62,9 +61,8 @@ const PasswordSection = function (props) {
           oldPassword: confirmedPassword,
           password: newPassword,
         };
-       sendPasswordData(sentData); 
+        sendPasswordData(sentData);
         // console.log(newConfirmedPassword, newPassword, password);
-        
       }
     }
   };
@@ -117,63 +115,6 @@ const PasswordSection = function (props) {
     document.querySelectorAll("form")[0].style.pointerEvents = "all";
   };
 
-  /**
-   * this function handle the event handler on edit button icon
-   * @type {function}
-   * @param {*} event
-   * @return {void} return nothing it just an event handler
-   */
-  const iconClick = (event) => {
-    document.querySelectorAll(".error-message").forEach((element) => {
-      element.style.transition = "0.5s .1s linear";
-    });
-    /**
-     * get all icon images
-     * @type {Array<Element>}
-     *
-     */
-    const imgs = document.querySelectorAll(`.${styles["icon-photo"]}`);
-    if (
-      event.target === imgs[1] ||
-      event.target.className === `${styles.dots}` ||
-      event.target.parentElement.className === `${styles.dots}`
-    ) {
-      document.getElementsByClassName(`${styles.dots}`)[0].style.display =
-        "none";
-      document
-        .querySelectorAll(`.${styles["password-box"]} .${styles.hidden}`)
-        .forEach((element) => {
-          element.classList.toggle(`${styles.hidden}`);
-          // if you click on the Email or on the Edit icon the Email box will apear and the confirm password box will appear too
-          // How I select this element? as regular selector .classX .classY{} then forEach one of them toggle the hidden class
-        });
-      document
-        .getElementsByClassName(`${styles["icon-photo"]}`)[1]
-        .classList.toggle(`${styles.hidden}`);
-      /**
-       * the form of the Account
-       * @type {Element}
-       *
-       */
-      const entireForm = document.getElementsByTagName("form")[0];
-      entireForm.style.pointerEvents = "none";
-      document.querySelectorAll("form >div").forEach((element) => {
-        element.style.opacity = "0.5";
-      });
-      /**
-       * get the section for changing password
-       * @type {Array<Element>}
-       *
-       */
-      const changePasswordSection = document.getElementsByClassName(
-        `${styles["password-box"]}`
-      )[0];
-      changePasswordSection.style.pointerEvents = "all";
-      changePasswordSection.style.opacity = "1";
-    }
-  };
-
-  
   return (
     <>
       <div data-testid="password-section" className={styles["password-box"]}>
--- src/components/Account/PasswordSection/PasswordSectionController.js
+++ src/components/Account/PasswordSection/PasswordSectionController.js
@@ -1,37 +1,94 @@
-
-
 import configureStore from "../../../redux/store";
-import { updateConfirmedPassword,updatenewPassword,updatenewConfirmedPassword } from "../../../redux/PasswordSection";
+import styles from "../Account.module.css";
+import {
+  updateConfirmedPassword,
+  updatenewPassword,
+  updatenewConfirmedPassword,
+} from "../../../redux/PasswordSection";
 /**
-   * this function handle any change in the states
-   * @type {function}
-   * @param {*} event
-   * @returns {void}
+ * this function handle any change in the states
+ * @type {function}
+ * @param {*} event
+ * @returns {void}
+ */
+const changeInput = (event) => {
+  document.querySelectorAll(".error-message").forEach((element) => {
+    // if the user enter invalid input then try to enter new values
+    element.style.visibility = "hidden";
+  });
+  document.querySelectorAll(".error-message").forEach((element) => {
+    // if the user enter invalid input then try to enter new values
+    element.style.visibility = "hidden";
+  });
+  if (event.target.id === "currentpassword") {
+    // updateInfo({ ...passwordInfo, confirmedPassword: event.target.value });
+    configureStore.dispatch(updateConfirmedPassword(event.target.value));
+  } else if (event.target.id === "newpassword") {
+    // updateInfo({ ...passwordInfo, newPassword: event.target.value });
+    configureStore.dispatch(updatenewPassword(event.target.value));
+  } else if (event.target.id === "confirmpassword") {
+    // updateInfo({ ...passwordInfo, newConfirmedPassword: event.target.value });
+    configureStore.dispatch(updatenewConfirmedPassword(event.target.value));
+  } else {
+    // updateInfo({ ...passwordInfo, confirmedPassword: event.target.value });
+    configureStore.dispatch(updateConfirmedPassword(event.target.value));
+  }
+  // console.log(passwordInfo);
+};
+
+/**
+ * this function handle the event handler on edit button icon
+ * @type {function}
+ * @param {*} event
+ * @return {void} return nothing it just an event handler
+ */
+const iconClick = (event) => {
+  document.querySelectorAll(".error-message").forEach((element) => {
+    element.style.transition = "0.5s .1s linear";
+  });
+  /**
+   * get all icon images
+   * @type {Array<Element>}
+   *
    */
- const changeInput = (event) => {
-    document.querySelectorAll(".error-message").forEach((element) => {
-      // if the user enter invalid input then try to enter new values
-      element.style.visibility = "hidden";
+  const imgs = document.querySelectorAll(`.${styles["icon-photo"]}`);
+  if (
+    event.target === imgs[1] ||
+    event.target.className === `${styles.dots}` ||
+    event.target.parentElement.className === `${styles.dots}`
+  ) {
+    document.getElementsByClassName(`${styles.dots}`)[0].style.display = "none";
+    document
+      .querySelectorAll(`.${styles["password-box"]} .${styles.hidden}`)
+      .forEach((element) => {
+        element.classList.toggle(`${styles.hidden}`);
+        // if you click on the Email or on the Edit icon the Email box will apear and the confirm password box will appear too
+        // How I select this element? as regular selector .classX .classY{} then forEach one of them toggle the hidden class
+      });
+    document
+      .getElementsByClassName(`${styles["icon-photo"]}`)[1]
+      .classList.toggle(`${styles.hidden}`);
+    /**
+     * the form of the Account
+     * @type {Element}
+     *
+     */
+    const entireForm = document.getElementsByTagName("form")[0];
+    entireForm.style.pointerEvents = "none";
+    document.querySelectorAll("form >div").forEach((element) => {
+      element.style.opacity = "0.5";
     });
-    document.querySelectorAll(".error-message").forEach((element) => {
-      // if the user enter invalid input then try to enter new values
-      element.style.visibility = "hidden";
-    });
-    if (event.target.id === "currentpassword") {
-      // updateInfo({ ...passwordInfo, confirmedPassword: event.target.value });
-      configureStore.dispatch(updateConfirmedPassword(event.target.value));
-    } else if (event.target.id === "newpassword") {
-      // updateInfo({ ...passwordInfo, newPassword: event.target.value });
-      configureStore.dispatch(updatenewPassword(event.target.value));
-    } else if (event.target.id === "confirmpassword") {
-      // updateInfo({ ...passwordInfo, newConfirmedPassword: event.target.value });
-      configureStore.dispatch(updatenewConfirmedPassword(event.target.value));
-    } else {
-      // updateInfo({ ...passwordInfo, confirmedPassword: event.target.value });
-      configureStore.dispatch(updateConfirmedPassword(event.target.value));
-    }
-    // console.log(passwordInfo);
-  };
+    /**
+     * get the section for changing password
+     * @type {Array<Element>}
+     *
+     */
+    const changePasswordSection = document.getElementsByClassName(
+      `${styles["password-box"]}`
+    )[0];
+    changePasswordSection.style.pointerEvents = "all";
+    changePasswordSection.style.opacity = "1";
+  }
+};
 
-
-export {changeInput}
+export { changeInput ,iconClick};
