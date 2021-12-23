586767b4a7161e918310d4f41a569911125197e7
omar214 <omarmohamed2322000@gmail.com>
2021-12-23 21:50:29 +0200

    dropdown

--- src/components/Layouts/NavBar/DropDownInbox.js
+++ src/components/Layouts/NavBar/DropDownInbox.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { NavLink } from 'react-router-dom';
 import { RiChatSmile3Fill } from 'react-icons/ri';
 import classes from './DropDownInbox.module.scss';
+import { getConversations } from './DropDownInboxService';
 
 // eslint-disable-next-line no-unused-vars
 const DropDownInbox = function (props) {
+  const [conversations, setConversations] = useState([]);
+  var x;
+  useEffect(async () => {
+    x= await getConversations();
+    setConversations(x.data);
+    console.log('x-->', x.data.length);
+    console.log('conversation -->', conversations);
+  },[])
+
   return (
     <div className={classes.smile}>
       <div className={classes.header}>
@@ -12,9 +22,26 @@ const DropDownInbox = function (props) {
         <NavLink to="/">New Message</NavLink>
       </div>
       <div className={classes.content}>
-        <RiChatSmile3Fill />
-        <span>Talk to tumblr</span>
-
+        { conversations.length ===0  &&  
+            (<>
+              <RiChatSmile3Fill />
+              <span>Talk to tumblr</span>
+            </>
+            )
+        }
+        { 
+          conversations.length > 0  && (
+            conversations.map((conversation) =>(
+              <>
+                <div className={classes.avatar}>
+                  {/* <img src={conversation.avatar} alt='avatar'/> */}
+                  tst
+                </div>
+                <div className={classes}></div>
+              </>
+            ))
+          )
+        } 
       </div>
     </div>
   );
