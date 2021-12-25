65658f676b1821f930dce526fc075ddf527c5246
Taher Mohamed Ahmed Saad <15126@stemegypt.edu.eg>
2021-12-25 21:28:08 +0200

    Fix refresh problem

--- src/api/api.js
+++ src/api/api.js
@@ -2,14 +2,18 @@
 import axios from 'axios';
 
 const baseURL = process.env.REACT_APP_API_URL;
-// eslint-disable-next-line prefer-const
-let headers = {};
-
-if (localStorage.getItem('token')) {
-  headers.Authorization = `${localStorage.getItem('token')}`;
-}
 
 export default axios.create({
   baseURL,
-  headers,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  transformRequest: [function (data, headers) {
+    
+    if(localStorage.getItem('token'))
+      headers['Authorization'] = `${localStorage.getItem('token')}`
+    // Do not change data
+
+    return JSON.stringify(data);;
+  }],
 });
