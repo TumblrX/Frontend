b23e92de98b9d6db0a75e40bf2035c5cc6dee80d
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-29 00:44:38 +0200

    Adding last touches on suggested list

--- src/components/ExploreSection/ExploreSuggestionList/ExploreSuggestionList.jsx
+++ src/components/ExploreSection/ExploreSuggestionList/ExploreSuggestionList.jsx
@@ -5,9 +5,7 @@ import img from "../../../assets/Images/icons8-chevron-right-60.png";
 import img1 from "../../../assets/Images/icons8-chevron-right-60.png";
 import { useEffect } from "react";
 import ExploreSuggestionListCard from "./ExploreSuggestionListCard";
-import api from "../../../api/api";
-import { setDataItems, setTag } from "../../../redux/suggestList";
-import confiugreStore from "../../../redux/store";
+import { getTags, onArrowClick } from "./ExploreSuggestionListServices";
 import { useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 /**
@@ -18,45 +16,9 @@ import { configureStore } from "@reduxjs/toolkit";
  */
 function ExploreSuggestionList() {
   const { items, tags } = useSelector((state) => state.suggestedList);
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    let config = {
-      headers: {
-        Authorization: token,
-      },
-    };
-    api
-      .get("api/user/get-tags", config)
-      .then((res) => {
-        // console.log("HIIIIIIIII", Object.values(res.data.tagsPhotos).length);
-        confiugreStore.dispatch(
-          setDataItems(Object.values(res.data.tagsPhotos))
-        );
-        // console.log(Object.keys(res.data.tagsPhotos));
-        confiugreStore.dispatch(setTag(Object.keys(res.data.tagsPhotos)));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  /**
-   * this function handle the click on the arrows on the suggestion list
-   * it just scrolls the suggestion list to right or left .
-   *
-   * @type {function}
-   * @param {*} event
-   * @returns {void}
-   */
-  const onArrowClick = (event) => {
-    let header = document.querySelector(`.${styles["suggestion-list"]}`);
-    let after = document.querySelector(`.${styles["after"]}`);
-    // console.log(event.target.parentElement);
-    if (event.target.parentElement === after) {
-      header.scrollLeft += 652;
-    } else {
-      header.scrollLeft -= 652;
-    }
-  };
+  let tago = [];
+  useEffect(getTags, []);
+
   return (
     <div className={styles["suggestion-list"]}>
       <div className={styles["before"]} onClick={onArrowClick}>
@@ -69,6 +31,7 @@ function ExploreSuggestionList() {
             <ExploreSuggestionListCard
               img1={value[0]}
               img2={value[0]}
+              key={index}
             />
           );
         else
@@ -76,75 +39,10 @@ function ExploreSuggestionList() {
             <ExploreSuggestionListCard
               img1={value[0]}
               img2={value[1]}
+              key={index}
             />
           );
       })}
-      {/* {Object.keys(items["0"]).map((value) => {
-        if (items["0"][value].length === 2) {
-          return (
-            <ExploreSuggestionListCard
-              tag={value}
-              img1={items["0"][value][0]}
-              img2={items["0"][value][1]}
-            />
-          );
-        } else {
-          return (
-            <ExploreSuggestionListCard
-              tag={value}
-              img1={items["0"][value][0]}
-              img2={items["0"][value][0]}
-            />
-          );
-        }
-      })} */}
-
-      {/* {Object.keys(items['0']).map((value)=>{
-        console.log(items['0'][value].length)
-        if(items['0'][value].length===2){
-          
-        }
-        else{
-
-        }
-      })} */}
-      {/* {Object.keys(items).map((value, index) => {
-        console.log(items['0']);
-        // console.log(items[value]);
-        if (items[value].length === 2)
-          return (
-            <ExploreSuggestionListCard
-              tag={value}
-              img1={items[value][0]}
-              img2={items[value][1]}
-            />
-          );
-        else
-          return (
-            <ExploreSuggestionListCard
-              tag={value}
-              img1={items[value][0]}
-              img2={items[value][0]}
-            />
-          );
-      })} */}
-
-      {/* <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard />
-      <ExploreSuggestionListCard /> */}
 
       <div className={styles["after"]} onClick={onArrowClick}>
         <img src={img1} alt="" />
