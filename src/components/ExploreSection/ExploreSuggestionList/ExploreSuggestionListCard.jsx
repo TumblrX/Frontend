5545af41aacd2354cecfab0aa346dc5591801077
Abdalla Mahmoud <47921768+Thebrownboy@users.noreply.github.com>
2021-12-29 18:15:22 +0200

    Integrating Trending list with the Backend

--- src/components/ExploreSection/ExploreSuggestionList/ExploreSuggestionListCard.jsx
+++ src/components/ExploreSection/ExploreSuggestionList/ExploreSuggestionListCard.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from "react";
-import img1 from "../../../assets/Images/Project_128-09.jpg";
+import img1 from "../../../assets/Images/avatar.png";
 import styles from "./scss/ExploreSuggestionList.module.scss";
 
 /**
@@ -14,8 +14,22 @@ function ExploreSuggestionListCard(props) {
     <div className={styles["suggestion-card"]}>
       <div className={styles["card-tag"]}>#{props.tag}</div>
       <div className={styles["card-imgs-container"]}>
-        <img src={props.img1} alt="" />
-        <img src={props.img2} alt="" />
+        <img
+          src={props.img1}
+          alt=""
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = img1;
+          }}
+        />
+        <img
+          src={props.img2}
+          alt=""
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = img1;
+          }}
+        />
       </div>
       <button>Follow</button>
     </div>
