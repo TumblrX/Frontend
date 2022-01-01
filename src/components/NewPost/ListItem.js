98639e7dad3e3c6d7b1deec12a59d13a59bfd8e0
Taher Mohamed Ahmed Saad <15126@stemegypt.edu.eg>
2022-01-01 04:51:05 +0200

    Minor change to make remove one hardcoded url

--- src/components/NewPost/ListItem.js
+++ src/components/NewPost/ListItem.js
@@ -9,8 +9,16 @@ import classes from './ListItem.module.scss';
 const ListItem = ({userBlog}) => {
   return (
     <div className={classes.item} value={userBlog.id}>
-      <div className={classes.avatar} value={userBlog.id}>
-        <img src={`http://tumblrx.me:3000/${userBlog.avatar}`}alt='avatar'value={userBlog.id}/>
+      <div className={classes.avatar} value={userBlog.id}>     
+        {           
+          userBlog?.avatar === 'none' ? ( 
+            <img src='https://assets.tumblr.com/images/default_avatar/sphere_open_128.png' alt="noavatar" value={userBlog.id} /> 
+          ) :userBlog?.avatar.includes("http") ? (
+            <img src={`${userBlog.avatar}`} alt="avatar" value={userBlog.id} />
+          ):(
+            <img src={`${process.env.REACT_APP_API_URL}/${userBlog.avatar}`} alt="avatar" value={userBlog.id} />
+          )
+        }
       </div>
       <div className={classes.text}value={userBlog.id}>
         {userBlog.handle}
